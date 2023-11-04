d96c4e46da403349342fd17a1e814f64cf7b777b
Example Name <example@email.com>
2023-11-04 17:49:57 +0000

    Removed TestFile

--- plugin/nvim-drupal-sh.lua
+++ plugin/nvim-drupal-sh.lua
@@ -6,7 +6,6 @@ end
 
 wk.register({
   ["<leader>t"] = { name = "+test" },
-  ["<leader>tt"] = { "<cmd>lua require'nvim-drupal-sh'.test_func()<cr>", "Run TestFunc" },
   ["<leader>tgc"] = { "<cmd>lua require'nvim-drupal-sh'.CloseAllFloatingWindows()<cr>", "Close All Floating Windows" },
   ["<leader>tgs"] = { "<cmd>lua require'nvim-drupal-sh'.getStandardServices()<cr>", "Show Standard Services" },
   ["<leader>tge"] = { ":lua require'nvim-drupal-sh'.serviceExists('')", "If Service Exists" },
